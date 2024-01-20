74a71c961469d5c75b55cd4554e995f343a9523b
Geisson Oliveira <geissonlucaso@gmail.com>
2024-01-20 15:40:35 -0300

    Aula sobre sintaxe e comentários no JavaScript.

---
+++ JoaoRibeiro/Mod5JavaScript/004SintaxeJS/script.js
@@ -0,0 +1,13 @@
+// Comentário.
+console.log('Console log')
+console.log("Teste semicolon");
+
+// Números em sequência.
+console.log(1)
+//console.log(2)
+console.log(3)
+
+/*
+Comentário multilinhas.
+Teste.
+*/
