a36b122cb587518bef92dce4b04ffc2dbb0427ec
Geisson Oliveira <geissonlucaso@gmail.com>
2024-01-20 17:05:23 -0300

    Aula sobre variáveis JavaScript.

---
+++ JoaoRibeiro/Mod5JavaScript/005Variaveis/script.js
@@ -0,0 +1,17 @@
+//var
+var nome = "Geisson Oliviera"
+var idade = 31
+var administrador = false
+console.log(nome)
+console.log(idade)
+console.log(administrador)
+
+var cidade
+cidade = "Belo horizonte"
+console.log(cidade)
+
+// let e const
+let endereco = "Rua HH"
+const tipoSangue = "A+"
+console.log(endereco)
+console.log(tipoSangue)
